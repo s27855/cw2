86bb500b8757edc6142f48f3bf460984b26b5936
s27855 <s27855@pjwstk.edu.pl>
2024-03-11 18:35:11 +0100

    Modyfikacja 2

--- cw2/cw2/Program.cs
+++ cw2/cw2/Program.cs
@@ -3,5 +3,8 @@
     public static void Main(string[] args)
     {
         Console.WriteLine("Hello");
+        Console.WriteLine("Goodbye");
+        Console.WriteLine("How are you?");
+        
     }
 }
