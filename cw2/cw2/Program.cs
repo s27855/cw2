a987b35711b6de223355fa9a80170d7e50c44677
s27855 <s27855@pjwstk.edu.pl>
2024-03-17 14:14:44 +0100

    metoda wyliczajaca srednia

--- cw2/cw2/Program.cs
+++ cw2/cw2/Program.cs
@@ -8,4 +8,22 @@
         Console.WriteLine("Good i guess");
         
     }
+    
+    public static double CalculateAverage(int[] array)
+    {
+        if (array == null || array.Length == 0)
+        {
+            throw new ArgumentException("Array must not be null or empty");
+        }
+
+        int sum = 0;
+        foreach (int num in array)
+        {
+            sum += num;
+        }
+
+        return (double)sum / array.Length;
+    }
+    
+
 }
--- cw2/cw2/obj/Debug/net8.0/cw2.AssemblyInfo.cs
+++ cw2/cw2/obj/Debug/net8.0/cw2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cw2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d4c5e8a2144687dd0657b3c76e2a1128ab6725b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c51bba21bcbc2e6a43638d84614c224ee704873b")]
 [assembly: System.Reflection.AssemblyProductAttribute("cw2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cw2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
