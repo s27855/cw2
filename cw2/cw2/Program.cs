c2594c4d15363a97a8f0991f63d23f56155bc51f
s27855 <s27855@pjwstk.edu.pl>
2024-03-17 14:36:51 +0100

    feature-max

--- cw2/cw2/Program.cs
+++ cw2/cw2/Program.cs
@@ -25,5 +25,18 @@
         return (double)sum / array.Length;
     }
     
+    public static int FindMax(int[] array)
+    {
+        if (array == null || array.Length == 0)
+            throw new ArgumentException("Array cannot be null or empty");
+
+        int max = array[0];
+        for (int i = 1; i < array.Length; i++)
+        {
+            if (array[i] > max)
+                max = array[i];
+        }
+        return max;
+    }
 
 }
